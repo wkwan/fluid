609d6ddecb9aa1ad537853a1ca431aa62eb08626
Will Kwan <wkwan94@gmail.com>
2025-05-19 21:29:20 -0700

    increase spawn density for future optimization testing

--- src/spawner.rs
+++ src/spawner.rs
@@ -29,7 +29,7 @@ impl Default for SpawnRegions {
                 size: Vec2::new(300.0, 200.0),
                 debug_color: Color::srgb(0.0, 0.0, 1.0),
             }],
-            spawn_density: 0.8,
+            spawn_density: 2.0,
             initial_velocity: Vec2::new(0.0, -50.0),
             jitter_strength: 10.0,
         }
